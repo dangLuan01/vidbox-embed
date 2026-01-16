55fbe03f48a267d329000308829596d55d72b9df
dangLuan01 <luanboss012@gmail.com>
2026-01-16 22:27:57 +0700

    update build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+
+  productionBrowserSourceMaps: false,
+  compress: true,
+  reactStrictMode: true,
+
+  experimental: {
+    serverActions: { 
+      bodySizeLimit: "2mb",
+    },
+  },
 };
 
 export default nextConfig;
